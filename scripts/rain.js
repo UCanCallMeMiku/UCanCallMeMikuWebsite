62367c8b4c31869165a48ee86a36ca4de8d87875
Konstantinos Katsimpas <79722468+UCanCallMeMiku@users.noreply.github.com>
2022-01-30 15:54:15 +0100

    fixed jittering scrollbar

--- scripts/rain.js
+++ scripts/rain.js
@@ -4,7 +4,7 @@ function rain() {
     var rainDiv = document.getElementById("rain");
     for (var i = 0; i<100; i+=(Math.random()/rainDensity)*(1500/window.innerWidth)) {
         var span = '<div class="raindrop" style="left:'+i*1+'%;animation-delay: '+Math.random()+'s;'+
-        'animation-duration: '+(Math.random()*2+0.5)+'s; bottom: '+(Math.random()+100)+'%;"></div>';
+        'animation-duration: '+(Math.random()*2+0.5)+'s; bottom: '+(101)+'%;"></div>';
         rainDiv.innerHTML += span;
     }
 } 
