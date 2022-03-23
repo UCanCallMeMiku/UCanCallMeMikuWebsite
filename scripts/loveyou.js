54a9655e519f894cbff04f0d723c1b1abe454e83
Miku <konsti032003@gmail.com>
2022-03-23 07:54:19 +0100

    cloudflare bug

--- scripts/loveyou.js
+++ scripts/loveyou.js
@@ -2,15 +2,6 @@ var rainDensity = 5;
 var duration = 2;
 var size = 1;
 
-function rain() {
-    var rainDiv = document.getElementById("rain");
-    for (var i = 0; i < 99; i += (Math.random() / rainDensity) * (1500 / window.innerWidth)) {
-        var span = '<i class="fa fa-heart heartdrop" style="font-size:' + ((Math.random() * 20) + 10) * size + 'px;color:red; left:' + i * 0.94 + '%;animation-delay: ' + Math.random() + 's;' +
-            'animation-duration: ' + (Math.random() * 2 + 0.5) * duration + 's; bottom: ' + (100) + '%;"></i>';
-        rainDiv.innerHTML += span;
-    }
-}
-
 
 var state = 0;
 var text = ["I", "LOVE", "COOKIE"];
@@ -23,5 +14,14 @@ function textNext() {
     } else {
         state++;
 
+    }
+}
+
+function rain() {
+    var rainDiv = document.getElementById("rain");
+    for (var i = 0; i < 99; i += (Math.random() / rainDensity) * (1500 / window.innerWidth)) {
+        var span = '<i class="fa fa-heart heartdrop" style="font-size:' + ((Math.random() * 20) + 10) * size + 'px;color:red; left:' + i * 0.94 + '%;animation-delay: ' + Math.random() + 's;' +
+            'animation-duration: ' + (Math.random() * 2 + 0.5) * duration + 's; bottom: ' + (100) + '%;"></i>';
+        rainDiv.innerHTML += span;
     }
 }
